1b6a70255d2185bb453230923e5dd30cdaa37977
Yann <yannpietroc@gmail.com>
2025-09-10 12:29:17 -0300

    5- Criando o SlideShow

---
+++ src/js/script.js
@@ -0,0 +1,47 @@
+// CRIANDO UM ARRAY DE OBJETO COM AS IMAGENS E INFORMAÇÕES DO BANNER
+
+const bannerItems = [
+  {
+    imagem:"./src/assets/Game1.jpg",
+    titulo:"Caçador de Bruxas",
+    descricao:"Caça bruxas com varinhas",
+  },
+
+  {
+    imagem:"./src/assets/Game2.jpg",
+    titulo:"Comedor de Códigos",
+    descricao:"Lancha funções como café da tarde",
+  },
+
+  {
+    imagem:"./src/assets/Game3.jpg",
+    titulo:"Coach City- Treino pra vida",
+    descricao:"Com grandes códfigos vem grandes projetos",
+  }
+];
+
+//DECLARANDO AS VARIÁVEIS ELEMENTOS COM DOM(DOCUMENT OBJECT MODEL)
+
+const tempo = 5000; //TEMPO EM MILISSEGUNDOS
+const elementoBanner = document.querySelector(".banner");
+const elementoTitulo = document.querySelector(".banner-titulo"); 
+const elementoDescricao = document.querySelector(".banner-descricao");
+
+let i = 0; // Inicio da contagem das imagens
+
+// Criando a função slideshow
+
+function slideShow() {
+    //ALTERA A IMAGEM DO FUNDO DO BANNER
+    elementoBanner.style.backgroundImage = `url(${bannerItems[i].imagem})`;
+    elementoTitulo.textContent = bannerItems[i].titulo;
+    elementoDescricao.textContent = bannerItems[i].descricao
+
+    i++;
+    if(i >= bannerItems.length){
+        i=0;
+    }
+    setTimeout(slideShow, tempo);
+
+}
+slideShow();
